318b05cdb1c9fd84c64fb84c46701ecc9e844902
Jonathan Greig <redteam316@gmail.com>
2013-08-29 08:36:47 +0000

    libembroidery time fix.

--- libembroidery/emb-time.c
+++ libembroidery/emb-time.c
@@ -1,8 +1,16 @@
 #include "emb-time.h"
+
+#ifdef ARDUINO
+/*TODO: arduino embTime includes */
+#else
 #include <time.h>
+#endif
 
 void embTime_initNow(EmbTime *t)
 {
+#ifdef ARDUINO
+/*TODO: arduino embTime_initNow */
+#else
     time_t rawtime;
     struct tm* timeinfo;
     time(&rawtime);
@@ -14,6 +22,7 @@ void embTime_initNow(EmbTime *t)
     t->hour   = timeinfo->tm_hour;
     t->minute = timeinfo->tm_min;
     t->second = timeinfo->tm_sec;
+#endif
 }
 
 /* kate: bom off; indent-mode cstyle; indent-width 4; replace-trailing-space-save on; */
