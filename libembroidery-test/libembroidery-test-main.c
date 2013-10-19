96b1c64597e723d578ba7d530d8adad08dd4ac63
Jonathan Greig <redteam316@gmail.com>
2013-10-19 15:33:15 -0500

    Initial libembroidery-test files.

---
+++ libembroidery-test/libembroidery-test-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "emb-reader-writer.h"
+
+int main(int argc, const char* argv[])
+{
+    /* Add tests here */
+    return 0;
+}
+
+/* kate: bom off; indent-mode cstyle; indent-width 4; replace-trailing-space-save on; */
