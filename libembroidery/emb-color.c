5f6f622a1b5bc755c1437e4769fdacda5a7da7c4
Jonathan Greig <redteam316@gmail.com>
2013-10-26 07:46:17 -0500

    Fixed broken embColor_fromHexStr function.

--- libembroidery/emb-color.c
+++ libembroidery/emb-color.c
@@ -1,4 +1,5 @@
 #include "emb-color.h"
+#include <stdlib.h>
 
 /* Returns an EmbColor. It is created on the stack. */
 EmbColor embColor_make(unsigned char r, unsigned char g, unsigned char b)
@@ -10,33 +11,17 @@ EmbColor embColor_make(unsigned char r, unsigned char g, unsigned char b)
     return stackColor;
 }
 
+/* Converts a 6 digit hex string (I.E. "00FF00") into an EmbColor and returns it. */
 EmbColor embColor_fromHexStr(char* val)
 {
-    /* TODO: pointer safety */
-
-    /* expect string beginning with 6 hex digits */
-    EmbColor color1;
-    int rgb = 0;
-    int i;
-    for(i = 0; i < 6; i++)
-    {
-        if(val[i] >= '0' && val[i] <= '9')
-        {
-            rgb = rgb * 16 + val[i] - '0';
-        }
-        else if(val[i] >= 'A' && val[i] <= 'F')
-        {
-            rgb = rgb * 16 + val[i] - 'A';
-        }
-        else if(val[i] >= 'a' && val[i] <= 'f')
-        {
-            rgb = rgb * 16 + val[i] - 'a';
-        }
-    }
-    color1.r = (unsigned char)((rgb >> 16) & 0xFF);
-    color1.g = (unsigned char)((rgb >> 8) & 0xFF);
-    color1.b = (unsigned char)(rgb & 0xFF);
-    return (color1);
+    EmbColor color;
+    char r[3] = { val[0], val[1], 0 };
+    char g[3] = { val[2], val[3], 0 };
+    char b[3] = { val[4], val[5], 0 };
+    color.r = (unsigned char)strtol(r, 0, 16);
+    color.g = (unsigned char)strtol(g, 0, 16);
+    color.b = (unsigned char)strtol(b, 0, 16);
+    return color;
 }
 
 /* kate: bom off; indent-mode cstyle; indent-width 4; replace-trailing-space-save on; */
