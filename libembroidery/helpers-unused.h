6994eec5cf50d669dee464783da4153a5e59ef3c
Jonathan Greig <redteam316@gmail.com>
2014-01-03 11:43:08 -0600

    Added missing helpers-unused.h file.

---
+++ libembroidery/helpers-unused.h
@@ -0,0 +1,8 @@
+#ifndef HELPERS_UNUSED_H
+#define HELPERS_UNUSED_H
+
+#define emb_unused(expr) do { (void)(expr); } while (0)
+
+#endif /* HELPERS_UNUSED_H */
+
+/* kate: bom off; indent-mode cstyle; indent-width 4; replace-trailing-space-save on; */
