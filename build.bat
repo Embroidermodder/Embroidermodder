5de18ff748f1a38ce7e1f876b0a775f02f994ad9
Robin Swift <robinswiftart@gmail.com>
2023-01-18 21:39:09 +0000

    build fixes

--- build.bat
+++ build.bat
@@ -8,11 +8,4 @@ cd build
 cmake -G "MinGW Makefiles" ..
 MinGW32-make.exe -j4
 cd ..
-copy assets\designs build\
-copy assets\docs build\
-copy assets\fonts build\
-copy assets\icons build\
-copy assets\images build\
-copy assets\samples build\
-copy assets\translations build\
-copy assets\config.toml build\
+xcopy assets build\ /E
