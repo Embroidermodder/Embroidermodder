51183181f28cb7b652275399d7fd5e6550af4f31
Robin Swift <87496244+robin-swift@users.noreply.github.com>
2023-02-13 21:48:25 +0000

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,13 @@
+git submodule init
+git submodule update
+
+mkdir build
+cd build
+cmake -G"MinGW Makefiles" .
+cmake --build .
+
+./embroidermodder.exe \
+  ../assets/samples/embroidermodder_logo/conflicts/Embroidermodder.DST \
+  ../assets/samples/shamrockin/shamrockin.dst \
+  ../assets/samples/spiral/spiral5.csv \
+  ../assets/samples/spiral/spiral6.csv
